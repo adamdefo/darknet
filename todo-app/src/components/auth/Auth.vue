71d88e501effda31ba9cf6f528f25e7631d6d6fe
aleksey <maylo.kondratev@yandex.ru>
2017-12-24 12:40:49 +0300

    Init

---
+++ todo-app/src/components/auth/Auth.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="auth">
+        <div class="auth__container">
+            <form @submit.prevent="onSubmit" class="auth__form">
+                <div class="auth__group">
+                    <div class="auth__group-item">
+                        <input v-model="login" class="auth__control" type="text" name="login" />
+                    </div>
+                    
+                </div>
+                <div class="auth__group-item">
+                    <div class="auth__group-item">
+                        <button @click="signIn" class="auth__button" type="button">
+                            <span>Login</span>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+  props: {},
+  model: {
+    login: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      login: ''
+    }
+  },
+  methods: {
+    signIn: function (event) {
+      alert(this.login)
+    },
+    onSubmit: function (event) {
+      alert('Submit')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.auth {
+  position: relative;
+  width: 100%;
+  min-width: 300px;
+  max-width: 460px;
+  &__container {
+    padding: 20px;
+  }
+  &__form {
+      position: relative;
+  }
+}
+</style>
---
+++ todo-app/src/components/navigation/Navigation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="navigation">
+    <nav class="nav nav-main">
+      <ul class="nav__list">
+        <li class="nav__item" v-for="link in navLinks">
+          <router-link :to="link.href" class="nav__link">{{ link.title }}</router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data () {
+    return {
+      navLinks: [
+        {
+          title: 'Main',
+          href: '/'
+        },
+        {
+          title: 'Orders',
+          href: '/orders'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
